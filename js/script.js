7c2ba99dc89dbabd1c8553f3b657f19059c08e33
emanelfahham <emanelfahham@gmail.com>
2022-06-25 00:33:49 +0200

    finished

--- js/script.js
+++ js/script.js
@@ -1,12 +1,10 @@
+// Movie Section:
 const apiKey = '6d1b5667232d91ab2a0410c07391e5e1';
 const baseUrl = 'https://api.themoviedb.org/3/movie/';
 
 let movieData = [];
 let searchedMoviesLocally = [];
 let dataType = 'now_playing';
-const nameRegx = /^[a-z]{3,}$/gi;
-const phoneRegex = /^(01)(0|1|2|5)[0-9]{8}$/;
-const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 // Selectors:
 let listItemsAnchor = Array.from(document.querySelectorAll('li a[data-target]'));
 const searchfromDBInput = document.querySelector('#searchfromDB');
@@ -61,11 +59,11 @@ $(document).ready(function () {
         } else {
              $('#sideBar').animate({left: 0},500, function(){
                 $('li').eq(0).animate({opacity:1, paddingTop:"10px"}, 500, function(){
-                    $('li').eq(1).animate({opacity:1, paddingTop:"10px"},500,function(){
-                        $('li').eq(2).animate({opacity:1, paddingTop:"10px"},500,function(){
-                            $('li').eq(3).animate({opacity:1, paddingTop:"10px"},500,function(){
-                                $('li').eq(4).animate({opacity:1, paddingTop:"10px"},500, function(){
-                                    $('li').eq(5).animate({opacity:1, paddingTop:"10px"},500)
+                    $('li').eq(1).animate({opacity:1, paddingTop:"10px"},600,function(){
+                        $('li').eq(2).animate({opacity:1, paddingTop:"10px"},650,function(){
+                            $('li').eq(3).animate({opacity:1, paddingTop:"10px"},700,function(){
+                                $('li').eq(4).animate({opacity:1, paddingTop:"10px"},700, function(){
+                                    $('li').eq(5).animate({opacity:1, paddingTop:"10px"},700)
                                 })
                             })
                         })
@@ -116,7 +114,93 @@ SearchfromLocalDataInput.addEventListener('keyup', searchLocally);
 getMovie();
 
 
+// contact Section:
+const nameRegx =/^[a-z]{2,}$/i;
+const phoneRegex = /^(\+201|01|00201)[0-2,5]{1}[0-9]{8}/;
+const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+const ageRegex = /^[1-9][0-9]$/;
+const passRegex = /^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$/;
+const nameInput = document.querySelector('#username');
+const emailInput = document.querySelector('#email');
+const phoneInput = document.querySelector('#phone');
+const ageInput = document.querySelector('#age');
+const passInput = document.querySelector('#password');
+const confirmPassInput = document.querySelector('#confirmPassword');
+const usernameErrorDiv = document.querySelector('.usernameErrorDiv');
+const emailErrorDiv = document.querySelector('.emailErrorDiv');
+const phoneErrorDiv = document.querySelector('.phoneErrorDiv');
+const AgeErrorDiv = document.querySelector('.AgeErrorDiv');
+const passErrorDiv = document.querySelector('.passErrorDiv');
+const confirmPassErrorDiv = document.querySelector('.confirmPassErrorDiv');
+const submitBtn = document.querySelector('button');
+const contactUsSection = document.querySelector('.contactUsSection');
+function validateUserName(e){
+    if(nameRegx.test(nameInput.value)){
+        usernameErrorDiv.style.display = 'none';
+        return true
+    } else {
+        usernameErrorDiv.style.display = 'block';
+        return false;
+    }
+}
+function validateEmail(){
+    if(emailRegex.test(emailInput.value)){
+        emailErrorDiv.style.display = 'none';
+        return true
+    } else {
+        emailErrorDiv.style.display = 'block';
+        return false;
+    }
+}
+function validatePhone(){
+    if(phoneRegex.test(phoneInput.value)){
+        phoneErrorDiv.style.display = 'none';
+        return true
+    } else {
+        phoneErrorDiv.style.display = 'block';
+        return false;
+    }
+}
+function validateAge(){
+    if(ageRegex.test(ageInput.value)){
+        AgeErrorDiv.style.display = 'none';
+        return true
+    } else {
+        AgeErrorDiv.style.display = 'block';
+        return false;
+    }
+}
+function validatePassword(){
+    if(passRegex.test(passInput.value)){
+        passErrorDiv.style.display = 'none';
+        return true
+    } else {
+        passErrorDiv.style.display = 'block';
+        return false;
+    }
+}
 
+function validateConfirmPass(){
+    if (passInput.value == this.value){
+        confirmPassErrorDiv.style.display = 'none';
+        return true;
+    } else {
+        confirmPassErrorDiv.style.display = 'block';
+        return false;
+    }
+}
 
-
-let userName=document.getElementById("name"),userEmail=document.getElementById("email"),userPhone=document.getElementById("phone"),userAge=document.getElementById("age"),userPassword=document.getElementById("password"),userRePassword=document.getElementById("rePassword"),userNameAlert=document.getElementById("namealert"),userEmailAlert=document.getElementById("emailalert"),userPhoneAlert=document.getElementById("phonealert"),userAgeAlert=document.getElementById("agealert"),userpasswordAlert=document.getElementById("passwordalert"),userRepasswordAlert=document.getElementById("repasswordalert");function userNameValid(){return 1==/^[a-zA-Z0-9]+$/.test(userName.value)?(userNameAlert.style.display="none",!0):(userNameAlert.style.display="block",!1)}function userEmailValid(){return 1==/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/.test(userEmail.value)?(userEmailAlert.style.display="none",!0):(userEmailAlert.style.display="block",!1)}function userPhoneValid(){return 1==/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/.test(userPhone.value)?(userPhoneAlert.style.display="none",!0):(userPhoneAlert.style.display="block",!1)}function userAgeValid(){return 1==/^[1-9][0-9]?$|^100$/.test(userAge.value)?(userAgeAlert.style.display="none",!0):(userAgeAlert.style.display="block",console.log("dkldkdlkdlk"),!1)}function userPasswordValid(){return 1==/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/.test(userPassword.value)?(userpasswordAlert.style.display="none",!0):(userpasswordAlert.style.display="block",!1)}function userRePasswordValid(){return userPassword.value==userRePassword.value?(userRepasswordAlert.style.display="none",!0):(userRepasswordAlert.style.display="block",!1)}userAgeAlert.style.display="none",userName.addEventListener("keyup",userNameValid),userEmail.addEventListener("keyup",userEmailValid),userPhone.addEventListener("keyup",userPhoneValid),userAge.addEventListener("keyup",userAgeValid),userPassword.addEventListener("keyup",userPasswordValid),userRePassword.addEventListener("keyup",userRePasswordValid),document.getElementById("contact").addEventListener("click",function(){userNameValid()&&userEmailValid()&&userPhoneValid()&&userAgeValid()&&userPasswordValid()&&userRePasswordValid()?document.getElementById("submitBtn").disabled=!1:document.getElementById("submitBtn").disabled=!0});
+function checkToSubmit(){
+    if( validateUserName() && validateEmail() && validatePhone() && validateAge() && validatePassword() && validateConfirmPass()){
+        submitBtn.classList.remove('disabled')
+    } else {
+        return false
+    }
+}
+nameInput.addEventListener('keyup', validateUserName );
+emailInput.addEventListener('keyup', validateEmail );
+phoneInput.addEventListener('keyup', validatePhone );
+ageInput.addEventListener('keyup', validateAge);
+passInput.addEventListener('keyup', validatePassword);
+confirmPassInput.addEventListener('keyup', validateConfirmPass)
+contactUsSection.addEventListener('click', checkToSubmit)
